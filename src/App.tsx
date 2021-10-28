4a284b4131afd3f75352228573ae5ba82420e153
Thiago Santos <thiagosantosdev@gmail.com>
2021-10-28 17:40:17 -0300

    carregando as transações

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { Dashboard } from './components/Dashboard';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 import { useState } from 'react';
 import Modal from 'react-modal';
 
@@ -19,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
@@ -28,6 +28,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle/>
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }

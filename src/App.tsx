0e59c7be6a234966fb740c4528a994e35719251a
Thiago Santos <thiagosantosdev@gmail.com>
2021-10-28 17:13:07 -0300

    utilizando o useContext no React

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { Dashboard } from './components/Dashboard';
+import { TransactionsContext } from './TransactionsContext';
 import { useState } from 'react';
 import Modal from 'react-modal';
 
@@ -18,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
@@ -27,6 +28,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle/>
-    </>
+    </TransactionsContext.Provider>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,13 @@
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
-
+import { useContext } from 'react';
 import { Container } from './styles';
+import { TransactionsContext } from '../../TransactionsContext';
 
 export function Summary() {
+  const data = useContext(TransactionsContext);
+
   return (
     <Container>
       <div>
